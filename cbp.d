710fa5fabca1590b77755021219edc69a0fb6a9f
spikespaz <spikespaz@outlook.com>
2018-04-12 16:43:23 -0700

    Call git to clone built remote

--- cbp.d
+++ cbp.d
@@ -1,7 +1,8 @@
-import std.stdio : writeln, stderr;
+import std.stdio : writeln, stdin, stdout, stderr;
 import std.array : join, split;
 import std.file : getcwd;
 import std.path : dirSeparator;
+import std.process : spawnShell, wait;
 
 int main(string[] args) {
     if (args.length == 1) {
@@ -10,14 +11,19 @@ int main(string[] args) {
     }
 
     string[] curr_path = split(getcwd(), dirSeparator);
-    string[] repo_name = split(args[1], "/");
+    string[] repo_name = split(args[1], '/');
     string[] repo_path;
 
     if (repo_name.length > 3) repo_path = repo_name;
     else
         repo_path = curr_path[$ - 3 + repo_name.length .. $] ~ repo_name;
 
-    writeln(repo_path);
+    string repo_url = "https://" ~ join(repo_path, '/') ~ ".git";
+    string git_cmd = "git clone " ~ join(repo_url ~ args[2 .. $]);
 
-    return 0;
+    writeln(git_cmd);
+
+    auto git_pid = spawnShell(git_cmd, stdin, stdout, stderr);
+
+    return wait(git_pid);
 }
