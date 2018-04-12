b1743f34dec74f2420d74e2c0f0c7ee2e4b30b51
spikespaz <spikespaz@outlook.com>
2018-04-12 14:46:49 -0700

    Write simple repo path builder

---
+++ cbp.d
@@ -0,0 +1,22 @@
+import std.stdio : writeln, stderr;
+import std.array : join, split;
+import std.file : getcwd;
+import std.path : dirSeparator;
+
+int main(string[] args) {
+    assert(args.length > 1, "Must pass at least the repository name.");
+
+    string[] curr_path = split(getcwd(), dirSeparator);
+    string[] repo_name = split(args[1], "/");
+    string[] repo_path;
+
+    if (repo_name.length > 3) {
+        repo_path = repo_name;
+    } else {
+        repo_path = curr_path[$ - 3 + repo_name.length .. $] ~ repo_name;
+    }
+
+    writeln(repo_path);
+
+    return 0;
+}
