cfaf4540743c0a2d19007cbffe90adff239082bc
spikespaz <spikespaz@outlook.com>
2018-04-12 14:50:58 -0700

    Add better error handling

--- cbp.d
+++ cbp.d
@@ -4,17 +4,18 @@ import std.file : getcwd;
 import std.path : dirSeparator;
 
 int main(string[] args) {
-    assert(args.length > 1, "Must pass at least the repository name.");
+    if (args.length == 1) {
+        stderr.writeln("Error: Must pass at least the repository name.");
+        return 1;
+    }
 
     string[] curr_path = split(getcwd(), dirSeparator);
     string[] repo_name = split(args[1], "/");
     string[] repo_path;
 
-    if (repo_name.length > 3) {
-        repo_path = repo_name;
-    } else {
+    if (repo_name.length > 3) repo_path = repo_name;
+    else
         repo_path = curr_path[$ - 3 + repo_name.length .. $] ~ repo_name;
-    }
 
     writeln(repo_path);
 
