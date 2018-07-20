13d9753cb3ed1ceafe40f2fa7694cb7a9e26b617
spikespaz <spikespaz@outlook.com>
2018-07-19 22:21:24 -0700

    Add ability to clone by only the current directory

--- cbp.d
+++ cbp.d
@@ -10,21 +10,25 @@ int main(string[] args) {
         return 1; // Exit with error
     }
 
-    string[] curr_path = split(getcwd(), dirSeparator); // Get cwd path segments
-    string[] repo_name = split(args[1], '/'); // Get repo path segments
+    string[] curr_path = getcwd().split(dirSeparator); // Get cwd path segments
+    string[] repo_name = args[1].split('/'); // Get repo path segments
     string[] repo_path;
 
-    if (repo_name.length > 3) repo_path = repo_name; // Includes more than domain, owner, repo
-    else repo_path = curr_path[$ - 3 + repo_name.length .. $] ~ repo_name; // Incomplete path, use parts of cwd
+    if (repo_name.length == 1 && repo_name[0] == ".")
+        repo_path = curr_path[$ - 3 .. $];
+    else
+        if (repo_name.length > 3) repo_path = repo_name; // Includes more than domain, owner, repo
+        else repo_path = curr_path[$ - 3 + repo_name.length .. $] ~ repo_name; // Incomplete path, use parts of cwd
 
-    string repo_url = "https://" ~ join(repo_path, '/') ~ ".git"; // Build the url of the repo from path
+    string repo_url = "https://" ~ repo_path.join('/') ~ ".git"; // Build the url of the repo from path
 
     const int start_arg = (args.length > 2 && args[2][0] != '-') + 1; // IF a clone path is provided second arg
-    string git_cmd = "git clone " ~ join(repo_url ~ args[start_arg .. $], ' '); // Build the command
+    string git_cmd = "git clone " ~ (repo_url ~ args[start_arg .. $]).join(' '); // Build the command
 
     writeln("> " ~ git_cmd); // Tell the user what the result is
 
-    auto git_pid = spawnShell(git_cmd, stdin, stdout, stderr); // Call that and get the pid
+    // auto git_pid = spawnShell(git_cmd, stdin, stdout, stderr); // Call that and get the pid
 
-    return wait(git_pid); // Wait for the clone to complete and return with pid exit status
+    // return wait(git_pid); // Wait for the clone to complete and return with pid exit status
+    return 1;
 }
