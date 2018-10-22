2f7d7f9c21303acec24493d7719ae7b4caa550e4
Jacob Birkett <spikespaz@outlook.com>
2018-10-21 21:26:53 -0700

    Add main function to python script

--- cbp.py
+++ cbp.py
@@ -3,28 +3,34 @@ from os.path import join, sep
 from subprocess import call
 from os import getcwd
 
-if len(argv) == 1:
-    print("Error: Must pass at least the repository name.", file=stderr)
-    exit(1)
 
-curr_path = getcwd().split(sep)
-repo_name = argv[1].split("/")
-repo_path = None
+def main():
+    if len(argv) == 1:
+        print("Error: Must pass at least the repository name.", file=stderr)
+        exit(1)
 
-if len(repo_name) == 1 and repo_name[0] == ".":
-    repo_path = curr_path[-3:]
-elif len(repo_name) >= 3:
-    repo_path = repo_name
-else:
-    repo_path = curr_path[len(repo_name) - 3:] + repo_name
+    curr_path = getcwd().split(sep)
+    repo_name = argv[1].split("/")
+    repo_path = None
 
-repo_url = "https://" + "/".join(repo_path) + ".git"
+    if len(repo_name) == 1 and repo_name[0] == ".":
+        repo_path = curr_path[-3:]
+    elif len(repo_name) >= 3:
+        repo_path = repo_name
+    else:
+        repo_path = curr_path[len(repo_name) - 3:] + repo_name
 
-start_arg = (len(argv) > 2 and argv[2][0] != "-") + 1
-git_cmd = ("git", "clone", repo_url, *argv[start_arg:])
+    repo_url = "https://" + "/".join(repo_path) + ".git"
 
-print(">", *git_cmd)
+    start_arg = (len(argv) > 2 and argv[2][0] != "-") + 1
+    git_cmd = ("git", "clone", repo_url, *argv[start_arg:])
 
-git_pid = call(git_cmd, shell=True)
+    print(">", *git_cmd)
 
-exit(git_pid)
+    git_pid = call(git_cmd, shell=True)
+
+    exit(git_pid)
+
+
+if __name__ == "__main__":
+    main()
